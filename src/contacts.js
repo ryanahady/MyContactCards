14e19fe4605c1bb942d0bdbeda4a27744d67e871
Ryan A <ryan.zad@gmail.com>
2021-01-28 12:41:18 +1100

    first commit

---
+++ src/contacts.js
@@ -0,0 +1,28 @@
+const contacts = [
+    {
+      id: 1,
+      name: "Jason Morgan",
+      imgURL: "images/jason.jpg",
+      phone: "0411 421 552",
+      email: "jason_morgan@gmail.com"
+    },
+    {
+      id: 2,
+      name: "Steve Carell",
+      imgURL:
+        "images/steve.jpg",
+      phone: "0481 931 471",
+      email: "michael_TheOffice@yahoo.com"
+    },
+    {
+      id: 3,
+      name: "Jessica Alba",
+      imgURL:
+        "images/jessica.jpg",
+      phone: "0488 999 231",
+      email: "jessicaA@gmail.com"
+    }
+  ];
+  
+  export default contacts;
+  
